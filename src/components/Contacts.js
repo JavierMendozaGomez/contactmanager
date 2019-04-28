50c46d228cf91de58269eb1eca22fa8c4b8fd4f9
javier Mendoza <javierenriquemendozagomez@gmail.com>
2019-04-28 17:59:20 +0100

    WIP

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,49 +1,26 @@
 import React, { Component } from 'react'
 import Contact from './Contact';
+import {Consumer} from '../context';
 
 class Contacts extends Component {
-    state = {
-        contacts: [
-            {
-                id: 1,
-                name: 'John Doe',
-                email: 'jdoe@gmail.com',
-                phone: '555-555-555',
-            },
-            {
-                id: 2,
-                name: 'Karen Smith',
-                email: 'karen@gmail.com',
-                phone: '444-444-444'
-            },
-            {
-                id: 3,
-                name: 'Henry Johnson',
-                email: 'henry@gmail.com',
-                phone: '111-111-111',
-            },
-            {
-                id: 4,
-                name: 'Jupiter Saturn',
-                email: 'Saturn@gmail.com',
-                phone: '222-222-222',
-            }
-        ]
-    }
-
     render() {
-        const { contacts } = this.state;
-
         return (
-            <React.Fragment>
-                {contacts.map(contact => (
-                    <Contact
-                        key={contact.id}
-                        contact={contact}
-                    />
-                ))}
-            </React.Fragment>
-        )
+            <Consumer>
+                {value => {
+                    const {contacts} = value;
+                    return (
+                    <React.Fragment>
+                        {contacts.map(contact => (
+                            <Contact
+                                key={contact.id}
+                                contact={contact}
+                            />
+                        ))}
+                    </React.Fragment>                        
+                    )
+                }}
+            </Consumer>
+        );
     }
 }
 

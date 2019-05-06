4664b3d433052daa966c92e61fc75bbdfbbbb83c
Javier <javierenriquemendozagomez@gmail.com>
2019-05-06 14:04:39 +0100

    6.33

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import AddContact from './components/contacts/AddContact';
-
-import {Provider} from './context.js'
+import About from './pages/About'
+import {Provider} from './context.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Provider>
-      <div className="App">
-        <Header branding='Contact Manager App' />
-        <div className='container'>
-          <AddContact />
-          <Contacts />
-        </div>
-      </div>
+        <Router>
+          <div className="App">
+            <Header branding='Contact Manager App' />
+            <div className='container'>
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/addContact" component={AddContact} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
       </Provider>
     );
   }
